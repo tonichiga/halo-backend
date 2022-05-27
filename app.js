d6118631ce666a0f73a67f045f6c49b431190442
Toni <tonichiga@gmail.com>
2022-05-27 03:34:56 +0300

    added helmet

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const express = require("express");
 const path = require("path");
 const logger = require("morgan");
 const indexRouter = require("./routes/index");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
 
 const app = express();
 
 // view engine setup
+app.use(cookieParser(123));
+app.use(helmet());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
