9f21030b79b613717ef40affdaa4266bb734a6af
Toni <tonichiga@gmail.com>
2022-05-27 03:40:42 +0300

    fix cors

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const logger = require("morgan");
 const indexRouter = require("./routes/index");
 const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
+const cors = require("cors");
 
 const app = express();
 
 // view engine setup
 app.use(cookieParser(123));
 app.use(helmet());
+app.use(cors());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
